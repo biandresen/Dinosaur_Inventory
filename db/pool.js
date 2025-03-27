b05439362a3490f17e3a4c5a2e43f14e75244cbd
birando <birger1@live.com>
2025-03-27 12:59:50 +0100

    config: change script

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,13 @@ const { Pool } = pkg;
 import "dotenv/config";
 
 export const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // host: process.env.PG_HOST,
+  // port: process.env.PG_PORT,
+  // database: process.env.PG_DATABASE,
+  // user: process.env.PG_USER,
+  // password: process.env.PG_PASSWORD,
 });
